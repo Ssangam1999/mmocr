f1dc09faaabb2020917d7bf2bc3b67f6cdf5dc9f
Ssangam1999 <sangam.poudel2016@gmail.com>
2023-10-11 21:47:15 +0545

    modified tools/infer.py

--- tools/infer.py
+++ tools/infer.py
@@ -7,7 +7,7 @@ from mmocr.apis.inferencers import MMOCRInferencer
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument(
-        'inputs', type=str, help='Input image file or folder path.')
+        '--inputs',default='/home/rujan/Documents/sangam/mmocr/MicrosoftTeams-image.png', type=str, help='Input image file or folder path.')
     parser.add_argument(
         '--out-dir',
         type=str,
@@ -16,42 +16,50 @@ def parse_args():
     parser.add_argument(
         '--det',
         type=str,
-        default=None,
+        default='/home/rujan/Documents/sangam/mmocr/configs/textdet/dbnetpp/dbnetpp_resnet50-oclip_fpnc_1200e_icdar2015.py',
         help='Pretrained text detection algorithm. It\'s the path to the '
         'config file or the model name defined in metafile.')
     parser.add_argument(
         '--det-weights',
         type=str,
-        default=None,
+        default='/home/rujan/Documents/sangam/mmocr/weights/DBNet/dbnetpp_resnet50-oclip_fpnc_1200e_icdar2015_20221101_124139-4ecb39ac.pth',
         help='Path to the custom checkpoint file of the selected det model. '
         'If it is not specified and "det" is a model name of metafile, the '
         'weights will be loaded from metafile.')
     parser.add_argument(
         '--rec',
         type=str,
-        default=None,
+        default='/home/rujan/Documents/sangam/mmocr/configs/textrecog/abinet/abinet_20e_st-an_mj.py',
+        #default=None,
         help='Pretrained text recognition algorithm. It\'s the path to the '
         'config file or the model name defined in metafile.')
     parser.add_argument(
         '--rec-weights',
         type=str,
-        default=None,
+        default='/home/rujan/Documents/sangam/mmocr/weights/ABINet/abinet_20e_st-an_mj_20221005_012617-ead8c139.pth',
+        # default=None,
         help='Path to the custom checkpoint file of the selected recog model. '
         'If it is not specified and "rec" is a model name of metafile, the '
         'weights will be loaded from metafile.')
     parser.add_argument(
         '--kie',
         type=str,
+        # default='/home/rujan/Documents/sangam/mmocr/configs/kie/sdmgr/sdmgr_unet16_60e_wildreceipt.py',
         default=None,
         help='Pretrained key information extraction algorithm. It\'s the path'
         'to the config file or the model name defined in metafile.')
     parser.add_argument(
         '--kie-weights',
         type=str,
+        # default='/home/rujan/Documents/sangam/mmocr/configs/kie/sdmgr/sdmgr_unet16_60e_wildreceipt.py',
         default=None,
         help='Path to the custom checkpoint file of the selected kie model. '
         'If it is not specified and "kie" is a model name of metafile, the '
         'weights will be loaded from metafile.')
+
+
+
+
     parser.add_argument(
         '--device',
         type=str,
@@ -62,18 +70,22 @@ def parse_args():
         '--batch-size', type=int, default=1, help='Inference batch size.')
     parser.add_argument(
         '--show',
+        default=True,
         action='store_true',
         help='Display the image in a popup window.')
     parser.add_argument(
         '--print-result',
+        default=True,
         action='store_true',
         help='Whether to print the results.')
     parser.add_argument(
         '--save_pred',
+        default=True,
         action='store_true',
         help='Save the inference results to out_dir.')
     parser.add_argument(
         '--save_vis',
+        default=True,
         action='store_true',
         help='Save the visualization results to out_dir.')
 
